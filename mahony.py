5d6dc170a3a9b479d9f806419468f2f367599d0d
gamblor21 <mark.komus@gmail.com>
2020-05-09 12:13:05 -0500

    Updated doc reference

--- mahony.py
+++ mahony.py
@@ -20,7 +20,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 """
-`ahrs`
+`mahony`
 ================================================================================
 
 AHRS library for CircuitPython
