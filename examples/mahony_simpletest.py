cd60762d90c695f985b16fb7c2778c4c7c3698a4
gamblor21 <mark.komus@gmail.com>
2020-05-13 13:18:46 -0500

    Added comments

--- examples/mahony_simpletest.py
+++ examples/mahony_simpletest.py
@@ -6,11 +6,11 @@
 ## Set to run on the Adafruit LSM9DS1 over I2C cause that is what I have
 ##
 
-import mahony
+import time
 import board
 import busio
-import time
 import adafruit_lsm9ds1
+import mahony
 
 MAG_MIN = [-0.5764, 0.0097, -0.5362]
 MAG_MAX = [0.4725, 0.9919, 0.4743]
@@ -29,16 +29,16 @@ def map_range(x, in_min, in_max, out_min, out_max):
     return min(max(mapped, out_max), out_min)
 
 
-## create the filter
-filter = mahony.Mahony(50, 5, 100)
+## create the ahrs_filter
+ahrs_filter = mahony.Mahony(50, 5, 100)
 
 # create the sensor
 i2c = busio.I2C(board.A3, board.A2)
 sensor = adafruit_lsm9ds1.LSM9DS1_I2C(i2c)
 
-count = 0   # used to count how often we are feeding the filter
-lastPrint = time.monotonic()    # last time we printed the yaw/pitch/roll values
-timestamp = time.monotonic_ns() # used to tune the frequency to approx 100 Hz
+count = 0  # used to count how often we are feeding the ahrs_filter
+lastPrint = time.monotonic()  # last time we printed the yaw/pitch/roll values
+timestamp = time.monotonic_ns()  # used to tune the frequency to approx 100 Hz
 
 while True:
     # on an Feather M4 approx time to wait between readings
@@ -64,22 +64,38 @@ while True:
         # read the accelerometer
         ax, ay, az = sensor.acceleration
 
-        # update the filter with the values
+        # update the ahrs_filter with the values
         # gz and my are negative based on my installation
-        filter.update(gx, gy, -gz, ax, ay, az, mx, -my, mz)
+        ahrs_filter.update(gx, gy, -gz, ax, ay, az, mx, -my, mz)
 
         count += 1
         timestamp = time.monotonic_ns()
 
-    # every 0.1 seconds print the filter values
+    # every 0.1 seconds print the ahrs_filter values
     if time.monotonic() > lastPrint + 0.1:
-        # filter values are in radians/sec multiply by 57.20578 to get degrees/sec
-        yaw = filter.yaw*57.20578
-        if (yaw < 0):   # adjust yaw to be between 0 and 360
+        # ahrs_filter values are in radians/sec multiply by 57.20578 to get degrees/sec
+        yaw = ahrs_filter.yaw * 57.20578
+        if yaw < 0:  # adjust yaw to be between 0 and 360
             yaw += 360
-        print("Orientation: ", yaw, ", ", filter.pitch*57.29578,", ", filter.roll*57.29578)
-        print("Quaternion: ", filter.q0, ", ", filter.q1, ", ", filter.q2, ", ", filter.q3)
+        print(
+            "Orientation: ",
+            yaw,
+            ", ",
+            ahrs_filter.pitch * 57.29578,
+            ", ",
+            ahrs_filter.roll * 57.29578,
+        )
+        print(
+            "Quaternion: ",
+            ahrs_filter.q0,
+            ", ",
+            ahrs_filter.q1,
+            ", ",
+            ahrs_filter.q2,
+            ", ",
+            ahrs_filter.q3,
+        )
 
-        #print("Count: ", count)    # optionally print out frequency
-        count = 0   # reset count
+        # print("Count: ", count)    # optionally print out frequency
+        count = 0  # reset count
         lastPrint = time.monotonic()
